10838bfcafc1e0a2a46a0cfc639cb47d4e999a10
rabbykhairul <rabbykhairul@gmail.com>
2021-10-21 21:52:08 +0600

    register route path to delete an author by id

--- src/routes/auther.js
+++ src/routes/auther.js
@@ -1,9 +1,10 @@
-const { createNewAuthor, updateAuthor } = require("../controllers/authorController");
+const { createNewAuthor, updateAuthor, deleteAuthor } = require("../controllers/authorController");
 const { isExistingAuthor } = require("../middlewares/author");
 
 const router = require("express").Router();
 
 router.post("/", isExistingAuthor, createNewAuthor);
 router.put('/:id', updateAuthor);
+router.delete("/:id", deleteAuthor);
 
 module.exports = router;
