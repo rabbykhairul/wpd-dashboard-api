91ab39f6db53fac771f1aff5e41b7402e2fa5810
rabbykhairul <rabbykhairul@gmail.com>
2021-10-22 00:22:49 +0600

    fix typo

--- src/models/Projects.js
+++ src/models/Projects.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
 
-const projectSchema = ({
+const projectSchema = mongoose.Schema({
   title: { type: String, required: true },
   budget: { type: Number, default: 0, min: 0 },
   status: { type: String, default: "Working" },
   completionPercentage: { type: Number, default: 0, min: 0, max: 100 }
-});
+}, { timestamps: true });
 
 module.exports = new mongoose.model("Projects", projectSchema);
