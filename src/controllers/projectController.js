d72b7b574a06bf53b13d74d3ea88fc27ef7e5bfa
rabbykhairul <rabbykhairul@gmail.com>
2021-10-21 23:46:44 +0600

    projectController - add method to update project

--- src/controllers/projectController.js
+++ src/controllers/projectController.js
@@ -35,4 +35,23 @@ const createNewProject = async (req, res) => {
   }
 }
 
-module.exports = { getProjects, createNewProject };
+const updateProject = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const updatedProject = await Projects.findOneAndUpdate({ _id: id }, req.body, { new: true });
+
+    return res.status(200).json({
+      success: true,
+      statusCode: 200,
+      message: "Project updated successfully",
+      project: updatedProject,
+    })
+  } catch (err) {
+    console.log("Error while updating project: ", err);
+    console.log("project payload: ", req.body);
+
+    return res.status(500).json({ success: false, statusCode: 500, message: "Internal server error. Please try again later"});
+  }
+}
+
+module.exports = { getProjects, createNewProject, updateProject };
