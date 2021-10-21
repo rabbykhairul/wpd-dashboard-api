09daf256eee472d6d5b4e5c95b9bba39eae1cde3
rabbykhairul <rabbykhairul@gmail.com>
2021-10-21 15:36:05 +0600

    authController - add method to handle user login requests

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,5 +1,5 @@
 const Users = require("../models/Users");
-const { getHashedPassowrd } = require("../utils/passwordMaster");
+const { getHashedPassowrd, comparePassword } = require("../utils/passwordMaster");
 const { generateToken } = require("../utils/tokenMaster");
 
 const handleUserRegistration = async (req, res) => {
@@ -26,4 +26,30 @@ const handleUserRegistration = async (req, res) => {
   }
 }
 
-module.exports = { handleUserRegistration };
+const handleUserLogin = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await Users.findOne({ email });
+    const plainUser = user?.toObject();
+
+    if (plainUser) {
+      const passwordMatched = await comparePassword(password, plainUser.password);
+      if (passwordMatched) {
+        delete plainUser.password;
+
+        return res.status(200).json({
+          success: true,
+          statusCode: 200,
+          message: "Login successfull",
+          user: plainUser,
+          token: generateToken(plainUser)
+        });
+      } else return res.status(400).json({ success: false, statusCode: 400, message: "Wrong email or password" });
+    } else return res.status(400).json({ success: false, statusCode: 400, message: "Wrong email or password" });
+  } catch (err) {
+    console.log("Error while logging in user: ", err);
+    return res.status(500).json({ success: false, statusCode: 500, message: "Something went wrong. Please try again later."});
+  }
+};
+
+module.exports = { handleUserRegistration, handleUserLogin };
