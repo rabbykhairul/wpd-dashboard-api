f4b8c2db82b331945d7ca7009f31012091babb3e
rabbykhairul <rabbykhairul@gmail.com>
2021-10-21 13:22:07 +0600

    create new users with hashed password instead of plain password

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,9 +1,14 @@
 const Users = require("../models/Users");
+const { getHashedPassowrd } = require("../utils/passwordMaster");
 
 const handleUserRegistration = async (req, res) => {
   try {
-    const newUser = new Users(req.body);
+    const { fullName, email, password: plainPassword } = req.body;
+    const password = await getHashedPassowrd(plainPassword);
+
+    const newUser = new Users({ fullName, email, password });
     await newUser.save();
+    
     return res.status(200).json({ success: true, statusCode: 200, message: "User registered successfully"});
   } catch (err) {
     console.log("Error while registering new user: ", err);
