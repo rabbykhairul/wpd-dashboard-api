65c3b5d18ca615d4ce2eb8087d5c2e41a6067407
rabbykhairul <rabbykhairul@gmail.com>
2021-10-21 21:34:07 +0600

    register route path for '/authors'

--- src/router.js
+++ src/router.js
@@ -1,8 +1,10 @@
 const authRoutes = require("./routes/auth");
+const authorRoutes = require("./routes/auther");
 const router = require("express").Router();
 
 router.get("/", (req, res) => res.send("<h1>Hello from purity-ui-api</h1>"));
 
 router.use("/auth", authRoutes);
+router.use("/authors", authorRoutes);
 
 module.exports = router;
