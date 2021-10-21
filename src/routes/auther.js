3b7af5e58e8a82a3377a131a506703d215f4ab1b
rabbykhairul <rabbykhairul@gmail.com>
2021-10-21 23:26:57 +0600

    register route path to get authors list

--- src/routes/auther.js
+++ src/routes/auther.js
@@ -1,8 +1,9 @@
-const { createNewAuthor, updateAuthor, deleteAuthor } = require("../controllers/authorController");
+const { createNewAuthor, updateAuthor, deleteAuthor, getAuthors } = require("../controllers/authorController");
 const { isExistingAuthor } = require("../middlewares/author");
 
 const router = require("express").Router();
 
+router.get("/", getAuthors);
 router.post("/", isExistingAuthor, createNewAuthor);
 router.put('/:id', updateAuthor);
 router.delete("/:id", deleteAuthor);
