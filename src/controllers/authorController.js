e4cbcc40fe6be24725b5b01a100a1fa30f52bce4
rabbykhairul <rabbykhairul@gmail.com>
2021-10-21 21:43:15 +0600

    authorController - add method to handle author update req

--- src/controllers/authorController.js
+++ src/controllers/authorController.js
@@ -19,4 +19,23 @@ const createNewAuthor = async (req, res) => {
   }
 }
 
+const updateAuthor = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const updatedAuthor = await Authors.findOneAndUpdate({ _id: id }, req.body, { new: true });
+
+    return res.status(200).json({
+      success: true,
+      statusCode: 200,
+      message: "Author updated successfully!",
+      author: updatedAuthor
+    })
+  } catch (err) {
+    console.log("Error while updating author: ", err);
+    console.log("author payload: ", req.body);
+
+    return res.status(500).json({ success: false, statusCode: 500, message: "Internal server error. Please try again later"});
+  }
+}
+
 module.exports = { createNewAuthor };
