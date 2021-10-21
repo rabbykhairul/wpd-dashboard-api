30d5cd9cc67ff7634d99f850689cd208839f7ee7
rabbykhairul <rabbykhairul@gmail.com>
2021-10-21 12:46:30 +0600

    use auth router for '/auth' requests

--- src/router.js
+++ src/router.js
@@ -1,5 +1,8 @@
+const authRoutes = require("./routes/auth");
 const router = require("express").Router();
 
 router.get("/", (req, res) => res.send("<h1>Hello from purity-ui-api</h1>"));
+
+router.use("/auth", authRoutes);
 
 module.exports = router;
