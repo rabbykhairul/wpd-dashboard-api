2f2b28e225766248ff9a7fe26647c73ea497209d
rabbykhairul <rabbykhairul@gmail.com>
2021-10-21 14:29:59 +0600

    upon registering new user, send a jwt token for immediate sign in

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,5 +1,6 @@
 const Users = require("../models/Users");
 const { getHashedPassowrd } = require("../utils/passwordMaster");
+const { generateToken } = require("../utils/tokenMaster");
 
 const handleUserRegistration = async (req, res) => {
   try {
@@ -8,8 +9,17 @@ const handleUserRegistration = async (req, res) => {
 
     const newUser = new Users({ fullName, email, password });
     await newUser.save();
+
+    const user = { fullName, email };
+    const token = generateToken(user);
     
-    return res.status(200).json({ success: true, statusCode: 200, message: "User registered successfully"});
+    return res.status(200).json({ 
+      success: true, 
+      statusCode: 200, 
+      message: "User registered successfully",
+      user,
+      token
+    });
   } catch (err) {
     console.log("Error while registering new user: ", err);
     return res.status(500).json({ success: false, statusCode: 500, message: "Internal server error. Please try again later."});
