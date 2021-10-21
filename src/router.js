0297b59fd029e90ff11496625bb2a368f965d188
rabbykhairul <rabbykhairul@gmail.com>
2021-10-21 23:30:36 +0600

    add support for route path '/projects'

--- src/router.js
+++ src/router.js
@@ -1,10 +1,12 @@
 const authRoutes = require("./routes/auth");
 const authorRoutes = require("./routes/auther");
+const projectRoutes = require("./routes/project");
 const router = require("express").Router();
 
 router.get("/", (req, res) => res.send("<h1>Hello from purity-ui-api</h1>"));
 
 router.use("/auth", authRoutes);
 router.use("/authors", authorRoutes);
+router.use("/projects", projectRoutes);
 
 module.exports = router;
