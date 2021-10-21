7dd1bd0aaff5045c0661f6d6c0f3078ac1796dab
rabbykhairul <rabbykhairul@gmail.com>
2021-10-22 00:05:09 +0600

    fix typo

--- src/models/Authors.js
+++ src/models/Authors.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const authorSchema = ({
+const authorSchema = mongoose.Schema({
   fullName: { type: String, required: true },
   email: { type: String, required: true },
   role: String,
