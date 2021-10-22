99bb63c8fa2bfb333f4bbb6958b57203aaf1a6b3
rabbykhairul <rabbykhairul@gmail.com>
2021-10-22 23:00:16 +0600

    auth middleware - add middleware to check user authentication

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,3 +1,4 @@
+const { validateToken } = require("../utils/tokenMaster");
 const Users = require("./../models/Users");
 
 const isDuplicateUser = async (req, res, next) => {
@@ -9,4 +10,16 @@ const isDuplicateUser = async (req, res, next) => {
   } else next();
 }
 
-module.exports = { isDuplicateUser };
+const isAuthenticated = async (req, res, next) => {
+  const bearerToken = req.get("Authorization")?.split(" ")[1];
+  const decodedToken = validateToken(bearerToken);
+
+  if (decodedToken) next();
+  else return res.status(400).json({
+    success: false,
+    statusCode: 400,
+    message: "Access denied. Try logging in or signing up."
+  })
+}
+
+module.exports = { isDuplicateUser, isAuthenticated };
