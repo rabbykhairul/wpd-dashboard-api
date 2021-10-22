a572d3513ebb1450dbe6011ca822552282495901
rabbykhairul <rabbykhairul@gmail.com>
2021-10-22 19:07:52 +0600

    fileMaster - add method to convert file data to base64 string

--- src/utils/fileMaster.js
+++ src/utils/fileMaster.js
@@ -22,4 +22,17 @@ const saveUploadedFile = async (file, folder = "/temp/") => {
   }
 )};
 
-module.exports = { saveUploadedFile };
+const getBase64String = (filePath, mimeType = "image/jpg") => {
+  console.log("\n---");
+  console.log("filePath: ", filePath);
+  console.log("---\n");
+
+  try {
+    const base64String = fs.readFileSync(filePath, { encoding: "base64" });
+    return `data:${mimeType};base64,${base64String}`;
+  } catch (err) {
+    return null;
+  }
+};
+
+module.exports = { saveUploadedFile, getBase64String };
