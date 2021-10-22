884f2088779e4d0080be84c4cfab6937592144ee
rabbykhairul <rabbykhairul@gmail.com>
2021-10-22 18:59:50 +0600

    create 'fileMaster' module to save uploaded file

---
+++ src/utils/fileMaster.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+const path = require("path");
+
+const saveUploadedFile = async (file, folder = "/temp/") => {
+  console.log("\n---");
+  console.log("file: ", file);
+  console.log("---\n");
+
+  return new Promise(async (resolve, reject) => {
+    const appRoot = require('app-root-path');
+
+    let ext = ".jpg";
+    ext = file.name.slice(file.name.lastIndexOf("."));
+
+    const filenamenew = file.name + '_' + Date.now() + ext;
+    const uploadPath = path.join(appRoot.path, folder + filenamenew);
+    file.mv(uploadPath, (err) => {
+      if (err) resolve(null);
+
+      resolve(uploadPath);
+    });
+  }
+)};
+
+module.exports = { saveUploadedFile };
